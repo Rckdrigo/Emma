c51a0d5b906515908744a67c07125d6a802971ff
Rckdrigo <rckdigomed@gmail.com>
2015-08-25 19:15:41 -0500

    Lvl 2

---
+++ Assets/VoiceManagerLvl2.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VoiceManagerLvl2 : MonoBehaviour {
+	
+	public AudioClip intro, tip, price;
+	AudioSource audioSource;
+	
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	public void PlayTip () {
+		audioSource.PlayOneShot(tip);
+	}
+	
+	public void PlayIntro(){
+		audioSource.PlayOneShot(intro);
+	}
+	
+	public void PlayPrice(){
+		audioSource.PlayOneShot(price);
+	}
+
+}
